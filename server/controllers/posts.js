0d7e838466be903abc79f733496c3898d4589350
Akash <akashleo2009@gmail.com>
2021-05-31 01:21:51 +0530

    frontend skeleton

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,9 +1,27 @@
 import PostMessage from "../models/postMessage.js";
 
-export const getPosts = (req, res) => {
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+
   res.send("This Works!");
 };
 
-export const createPost = (req, res) => {
-  res.send("This Works!");
+export const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+
+  res.send("post Creation!");
 };
