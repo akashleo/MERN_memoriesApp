9299c2f57c5dbdd979e9bd07deb4a2f1d580078a
Akash <akashleo2009@gmail.com>
2021-06-24 01:31:26 +0530

    update functionality

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -29,7 +29,16 @@ const Form = ({ currentId, setCurrentId }) => {
     selectedFile: "",
   });
 
-  const clear = () => {};
+  const clear = () => {
+    setCurrentId(null);
+    setPostData({
+      creator: "",
+      title: "",
+      message: "",
+      tags: "",
+      selectedFile: "",
+    });
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -38,6 +47,7 @@ const Form = ({ currentId, setCurrentId }) => {
     } else {
       dispatch(createPost(postData));
     }
+    clear();
   };
   return (
     <Paper className={classes.paper}>
@@ -46,69 +56,76 @@ const Form = ({ currentId, setCurrentId }) => {
         noValidate
         className={`${classes.root} ${classes.form}`}
         onSubmit={handleSubmit}
-      ></form>
-      <Typography variant="h6">Creating a Memory</Typography>
-      <TextField
-        name="creator"
-        variant="outlined"
-        label="Creator"
-        fullWidth
-        value={postData.creator}
-        onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
-      />
-      <TextField
-        name="title"
-        variant="outlined"
-        label="Title"
-        fullWidth
-        value={postData.title}
-        onChange={(e) => setPostData({ ...postData, title: e.target.value })}
-      />
-      <TextField
-        name="message"
-        variant="outlined"
-        label="Message"
-        fullWidth
-        value={postData.message}
-        onChange={(e) => setPostData({ ...postData, message: e.target.value })}
-      />
-      <TextField
-        name="tags"
-        variant="outlined"
-        label="Tags"
-        fullWidth
-        value={postData.tags}
-        onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
-      />
-      <div className={classes.fileInput}>
-        <FileBase
-          type="file"
-          multiple={false}
-          onDone={({ base64 }) =>
-            setPostData({ ...postData, selectedFile: base64 })
+      >
+        <Typography variant="h6">
+          {currentId ? `Editing` : `Creating`} a Memory
+        </Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
           }
         />
-      </div>
-      <Button
-        className={classes.buttonSubmit}
-        variant="contained"
-        color="primary"
-        size="large"
-        type="submit"
-        fullWidth
-      >
-        Submit
-      </Button>
-      <Button
-        variant="contained"
-        color="secondary"
-        size="small"
-        onClick={clear}
-        type="submit"
-        fullWidth
-      >
-        Clear
-      </Button>
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullWidth
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        />
+        <TextField
+          name="message"
+          variant="outlined"
+          label="Message"
+          fullWidth
+          value={postData.message}
+          onChange={(e) =>
+            setPostData({ ...postData, message: e.target.value })
+          }
+        />
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags"
+          fullWidth
+          value={postData.tags}
+          onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+        />
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) =>
+              setPostData({ ...postData, selectedFile: base64 })
+            }
+          />
+        </div>
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={clear}
+          type="submit"
+          fullWidth
+        >
+          Clear
+        </Button>
+      </form>
     </Paper>
   );
 };
