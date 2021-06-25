a8971f6050df81b1fe20daf3265c0940b30e1937
Akash <akashleo2009@gmail.com>
2021-06-25 18:39:00 +0530

    like functionality

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -21,7 +21,7 @@ export const createPost = (post) => async (dispatch) => {
 
 export const updatePost = (id, post) => async (dispatch) => {
   try {
-    const { data } = await api.updatePost(post);
+    const { data } = await api.updatePost(id, post);
     dispatch({ type: "UPDATE", payload: data });
   } catch (error) {
     console.log(error);
@@ -33,6 +33,15 @@ export const deletePost = (id) => async (dispatch) => {
     await api.deletePost(id);
 
     dispatch({ type: "DELETE", payload: id });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const likePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id);
+    dispatch({ type: "UPDATE", payload: data });
   } catch (error) {
     console.log(error);
   }
