4022f2ad21718a9f29756ec8d5975fb641fb572d
Akash <akashleo2009@gmail.com>
2021-06-26 00:29:53 +0530

    like modification

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -51,7 +51,7 @@ const Post = ({ post, setCurrentId }) => {
         {post.title}
       </Typography>
       <CardContent>
-        <Typography variant="h5" gutterBottom>
+        <Typography variant="body2" gutterBottom>
           {post.message}
         </Typography>
       </CardContent>
