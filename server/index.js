d29d60406929b9847ad2beec8faa689058a96a01
Akash <akashleo2009@gmail.com>
2021-06-27 21:04:53 +0530

    final app before deployment

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -12,13 +14,12 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://akashleo:akash10898@cluster0.raiwo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+//const CONNECTION_URL =  "mongodb+srv://akashleo:akash10898@cluster0.raiwo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
